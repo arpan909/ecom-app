648543f5e37ff5f13d30e64c3bd9c63ede4ac0d2
unknown <arpanbhatt909@gmail.com>
2020-09-08 18:59:22 +0530

    split the homepage into components and also addes some stylings

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.styles.scss";
+import Directory from "../components/directory-menu/directory.component";
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory></Directory>
+    </div>
+  );
+};
+
+export default HomePage;
