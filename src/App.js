a3becdc238a8176805e3491d1c7a90361be2e001
unknown <arpanbhatt909@gmail.com>
2020-09-21 14:49:14 +0530

    add routing to menu-list with HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage.component';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homepage.component";
+
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>Hats Page:{props.match.params.id}</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact component={HomePage} path="/" />
+        <Route component={HatsPage} path="/hats" />
+      </Switch>
     </div>
   );
 }
